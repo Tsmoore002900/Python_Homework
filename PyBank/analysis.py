05ac8b012d441e95f5457608138392549fb3f35e
Tsmoore002900 <tgem9914@gmail.com>
2023-01-10 21:25:47 -0500

    Created outline for what need to be done in each Python

---
+++ PyBank/analysis.py
@@ -0,0 +1,16 @@
+import os
+import csv
+# Path to collect data from the Resources folder
+graduation_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+#Insert Finacial Analysis as text
+
+# Total Months Calculation
+
+#Total Calculation
+
+#Average Change Calculation
+
+#Greatest Increase in Profits Calculation
+
+#Greatest Decrease in Profits Calculation
---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+import os
+import csv
+# Path to collect data from the Resources folder
+graduation_csv = os.path.join('..', 'Resources', 'election_data.csv')
+
+#Election Results
+
+#Total Votes
+
+#Total Votes Per Candidate
+
+#Winner
