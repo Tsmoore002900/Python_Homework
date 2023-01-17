c95753490e5c8d38c3f3d02be932a995b212e2d6
Tsmoore002900 <tgem9914@gmail.com>
2023-01-16 22:11:54 -0500

    Update python for PyBank and added txt.file

--- PyBank/analysis.py
+++ PyBank/analysis.py
@@ -2,35 +2,57 @@ import os
 import csv
 # Path to collect data from the Resources folder
 graduation_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+#path to save
+file_to_save = os.path.join('budget_analysis.txt')
 
 # List to store data
-Date = []
-Profit_Losses = []
-with open(graduation_csv) as csvfile:
+months = []
+profit_loss_changes = []
+count_months = 0
+net_profit_loss = 0
+previous_month_profit_loss = 0
+current_month_profit_loss = 0
+profit_loss_change = 0
+
+with open(graduation_csv, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     csv_header =next(csv_reader)
-    #print(csv_reader)
-    #print(f"CSV Header: {csv_header}")
+
     for row in csv_reader:
-        Date.append(str(row[0]))
-        Profit_Losses.append(int(row[1]))
-        # Total Months Calculation
-        Total_Months = len(Profit_Losses)
-        # Total Calculation
-        Total = sum(Profit_Losses)
-        #Average Change Calculation
-        Average_Change = round((Total / Total_Months), 2)
-        #Greatest Increase in Profits Calculation
-        Increase = max(Profit_Losses)
-        #Greatest Decrease in Profits Calculation
-        Decrease = max(Profit_Losses)
+        count_months += 1
+        current_month_profit_loss = int(row[1])
+        net_profit_loss += current_month_profit_loss
         
-    #Insert Finacial Analysis as text
-    print("Financial Analysis")
-    print("-----------------------------------")    
-    print(f"Total Months: {str(Total_Months)}")
-    print(f"Total: {str(Total)}")
-    print(f"Average Change: {str(Average_Change)}")
-    print(f"Greatest Increase in Profits: {str(Increase)}")
-    print(f"Greatest Decrease in Profits: {str(Decrease)}")
+        if (count_months == 1):
+            previous_month_profit_loss = current_month_profit_loss
+            continue
+        else:
+            profit_loss_change = current_month_profit_loss - previous_month_profit_loss
+            months.append(row[0])
+            profit_loss_changes.append(profit_loss_change)
+            previous_month_profit_loss = current_month_profit_loss
+    
+    # Calculate Changes
+    sum_profit_loss = sum(profit_loss_changes)
+    average_profit_loss = round(sum_profit_loss / (count_months - 1), 2)
+    greatest_increase = max(profit_loss_changes)
+    greatest_decrease = min(profit_loss_changes)
+
+    greatest_increase_index = profit_loss_changes.index(greatest_increase)
+    greatest_decrease_index = profit_loss_changes.index(greatest_decrease)
+    best_month = months[greatest_increase_index]
+    worst_month = months[greatest_decrease_index]
+
+    #print
+    with open(file_to_save, "w") as txt_file:
+        bank_analysis = (
+            f"\nFinacial Analysis\n"
+            f"------------------------------\n"
+            f"Total Months: {count_months}\n"
+            f"Total: ${net_profit_loss}\n"
+            f"Average Change: ${average_profit_loss}\n"
+            f"Greatest Increase in Profits: {best_month} ${greatest_increase}\n"
+            f"Greatest Decrease in Profits: {worst_month} ${greatest_decrease}\n")
+        print(bank_analysis)
+        txt_file.write(bank_analysis)
 
