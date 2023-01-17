c33497bf3d53eda86fada8123e1e44fbd31d4c70
Tsmoore002900 <tgem9914@gmail.com>
2023-01-16 20:00:51 -0500

    PyPoll upload plus added txt.file

--- PyBank/analysis.py
+++ PyBank/analysis.py
@@ -3,14 +3,34 @@ import csv
 # Path to collect data from the Resources folder
 graduation_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 
-#Insert Finacial Analysis as text
+# List to store data
+Date = []
+Profit_Losses = []
+with open(graduation_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    csv_header =next(csv_reader)
+    #print(csv_reader)
+    #print(f"CSV Header: {csv_header}")
+    for row in csv_reader:
+        Date.append(str(row[0]))
+        Profit_Losses.append(int(row[1]))
+        # Total Months Calculation
+        Total_Months = len(Profit_Losses)
+        # Total Calculation
+        Total = sum(Profit_Losses)
+        #Average Change Calculation
+        Average_Change = round((Total / Total_Months), 2)
+        #Greatest Increase in Profits Calculation
+        Increase = max(Profit_Losses)
+        #Greatest Decrease in Profits Calculation
+        Decrease = max(Profit_Losses)
+        
+    #Insert Finacial Analysis as text
+    print("Financial Analysis")
+    print("-----------------------------------")    
+    print(f"Total Months: {str(Total_Months)}")
+    print(f"Total: {str(Total)}")
+    print(f"Average Change: {str(Average_Change)}")
+    print(f"Greatest Increase in Profits: {str(Increase)}")
+    print(f"Greatest Decrease in Profits: {str(Decrease)}")
 
-# Total Months Calculation
-
-#Total Calculation
-
-#Average Change Calculation
-
-#Greatest Increase in Profits Calculation
-
-#Greatest Decrease in Profits Calculation
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,62 @@
+#add dependencies
 import os
 import csv
+
 # Path to collect data from the Resources folder
-graduation_csv = os.path.join('..', 'Resources', 'election_data.csv')
+election_data = os.path.join('..', 'Resources', 'election_data.csv')
+# path to save
+file_to_save = os.path.join('election_analysis.txt')
 
-#Election Results
+#List to store Data
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
-#Total Votes
+#Open and read the file. 
+with open(election_data) as election_data:
+    file_reader = csv.reader(election_data) 
+    headers = next(file_reader)
+    
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
 
-#Total Votes Per Candidate
+        if candidate_name not in candidate_options: 
+            candidate_options.append(candidate_name) 
+            candidate_votes[candidate_name] = 0 
+        candidate_votes[candidate_name] += 1
 
-#Winner
+#Save the results to text file. 
+with open(file_to_save, "w") as txt_file: 
+    election_data = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"--------------------------\n")
+    print(election_data, end="")
+    txt_file.write(election_data)
+
+    for candidate_name in candidate_votes: 
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results = (f"{candidate_name}: {vote_percentage:.3f}% ({votes})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+     
+
+        #Determine winning vote count & candidate
+        if (votes > winning_count) and (vote_percentage > winning_percentage): 
+            winning_count = votes
+            winning_percentage = vote_percentage 
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-----------------------\n")
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
+    
